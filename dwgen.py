6cb4873617d5ac9cf7c6d005245c621bd5390b06
Pete Goodall <pete@digicratic.com>
2020-03-31 11:45:54 +0100

    - Removed all debug print statements - Printing all words on the same line - Printing words in blue to distinguish from other terminal output

--- dwgen.py
+++ dwgen.py
@@ -9,10 +9,8 @@ import sqlite3
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-print("Read in the file from the command line...")
+# print("Read in the file from the command line...")
 L = sys.stdin.read()
-#words = []
-#filtered_words = []
 stop_words = set(stopwords.words('english'))
 home = os.path.expanduser("~")
 localdir = home + "/.dwgen"
@@ -26,31 +24,32 @@ def createLocalStorage():
     print("Created local storage at %s" % localdir)
 
 # Check if the localdir already exists
-print("Checking if the local storage directory exists at %s ..." % localdir)
+# print("Checking if the local storage directory exists at %s ..." % localdir)
 if not os.path.exists(localdir):
   print("Local directory doesn't exist. Creating %s ..." % localdir)
   createLocalStorage()
-else:
-  print("Local directory %s already exists ..." % localdir)
+# else:
+#   print("Local directory %s already exists ..." % localdir)
 
 # Connect to local storage
-print("Checking for local db ...")
+# print("Checking for local db ...")
 db_file = localdir + '/db.sqlite3'
 try:
   db = sqlite3.connect(db_file)
 except OSError:
   sys.exit("Failed to create local DB for storage.")
 
-if os.path.exists(db_file):
-  print("DB at %s ..." % db_file)
-else:
+if not os.path.exists(db_file):
   sys.exit("DB file missing. What happened?")
 
 words = (word_tokenize(L))
 words = [word.lower() for word in words if not word in stop_words]
 words = [word for word in words if word.isalpha()]
+words = [word for word in words if len(word) > 3 & len(word) < 9]
 
 random.shuffle(words)
-print("Returning four random words:")
-for count in range(4):
-  print(words[count])
+print("Returning five random words:")
+for count in range(5):
+  print('\033[1;34;40m %s' % words[count], end = ' ')
+
+print('\033[1;37;40m \n')
