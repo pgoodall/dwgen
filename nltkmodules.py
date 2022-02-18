7e1cd70c468943d8e6e4e666a083f5aa1618028d
Peter Goodall <peter.goodall@panaseer.com>
2022-02-18 17:17:23 +0000

    Just added a line to supress nltk.download output

--- nltkmodules.py
+++ nltkmodules.py
@@ -1,4 +1,4 @@
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('perluniprops')
+nltk.download('stopwords', quiet=True)
+nltk.download('punkt', quiet=True)
+nltk.download('perluniprops', quiet=True)
